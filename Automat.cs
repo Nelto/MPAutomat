6a6d0888336c68c686692e1d75267d0f7ab10f31
Nelto <32131337+Nelto@users.noreply.github.com>
2018-11-10 13:39:21 +0300

    v1.1
    MpAutomat completed

--- Automat.cs
+++ Automat.cs
@@ -12,34 +12,13 @@ namespace MPautomat
         private Dictionary<string, string[]> functions = new Dictionary<string, string[]>();
         private List<string> inputstring = new List<string>();
         private List<string> score = new List<string>();
-        private int step = 0;
-        private bool isRecognition = false;
+        private int step;
+        private bool isRecognition;
 
 
-        public Automat()
+        public Automat(string start)
         {
-            string rule;
- 
-            Console.WriteLine("Введите правила грамматики. Каждое правило - на отдельной строке. Левая часть не должна повторяться\n" +
-                "Пример строки: S->aB\n" +
-                "Для символа пустой строки (эпсилон) зарезервирован символ 'e'.\n" +
-                "Для завершения ввода введите пустую строку.");
-            for (; ; )
-            {
-                rule = Console.ReadLine();
-                if (rule == string.Empty || rule == "") break;
-                
-            }
-            Console.WriteLine("Введите начальный символ грамматики");
-            score.Add(Console.ReadLine());
-            Console.WriteLine("Введите строку для распознования");
-            inputstring.Add(Console.ReadLine());
-            recognitionString(score[0]);
-            for(int i =0; i < score.Count; i++)
-            {
-                Console.WriteLine( $"{i}  {inputstring[i]}  {score[i]}");
-            }
-            Console.Read();
+            score.Add(start);
         }
 
         public void AddRule(string rule)
@@ -52,12 +31,12 @@ namespace MPautomat
         private void Shift()
         {
             score[step] = score[step].Replace("e", string.Empty);
-                while (inputstring[step] != string.Empty && score[step]!=string.Empty && inputstring[step][0] == score[step][0])
-                {
-                    inputstring.Add(inputstring[step].Remove(0, 1));
-                    score.Add(score[step].Remove(0, 1));
-                    step++;
-                }
+            while (inputstring[step] != string.Empty && score[step] != string.Empty && inputstring[step][0] == score[step][0])
+            {
+                inputstring.Add(inputstring[step].Remove(0, 1));
+                score.Add(score[step].Remove(0, 1));
+                step++;
+            }
             if (score[step] == string.Empty && inputstring[step] == string.Empty) isRecognition = true;
         }
 
@@ -65,8 +44,8 @@ namespace MPautomat
         {
             while (!inputstring[step].Equals(inputstring[step - 1]))
             {
-                inputstring.RemoveAt(inputstring.Count-1);
-                score.RemoveAt(score.Count-1);
+                inputstring.RemoveAt(inputstring.Count - 1);
+                score.RemoveAt(score.Count - 1);
                 step--;
             }
             inputstring.RemoveAt(inputstring.Count - 1);
@@ -77,40 +56,60 @@ namespace MPautomat
         private bool TermCount()
         {
             int termCount = 0;
-            for (int i =0; i < score[step].Length; i++)
+            for (int i = 0; i < score[step].Length; i++)
             {
                 if (score[step][i] < 'A' || score[step][i] > 'Z') termCount++;
             }
             return termCount > inputstring[step].Length;
         }
 
-        private void recognitionString(string notTerm)
+        private void Recognition(string notTerm)
         {
             for (int i = 0; i < functions[notTerm].Length; i++)
             {
                 if (isRecognition) break;
                 if (Regex.IsMatch(score[step], "^[A-Z]") || functions[notTerm][i] == "e")
                 {
-                    score.Add((functions[notTerm][i]+score[step].Substring(1, score[step].Length-1)));
+                    score.Add((functions[notTerm][i] + score[step].Substring(1, score[step].Length - 1)));
                     inputstring.Add(inputstring[step]);
                     step++;
                     Shift();
                     if (!isRecognition)
                     {
-                        if (Regex.IsMatch(score[step], "^[A-Z]")&&!TermCount()) recognitionString(Convert.ToString(score[step][0]));
+                        if (Regex.IsMatch(score[step], "^[A-Z]") && !TermCount()) Recognition(Convert.ToString(score[step][0]));
                         else ReverseShift();
                     }
                 }
             }
-            if (!isRecognition && notTerm!=score[0])
+            if (!isRecognition && notTerm != score[0])
             {
                 ReverseShift();
             }
         }
 
-        public void RecognitionResult()
+        private void Print()
         {
+            Console.WriteLine(" {0} | {1} | {2}", "Шаг", "Входная строка", "Содержимое магазина");
+            for (int i = 0; i < step + 1; i++)
+            {
+                Console.WriteLine("{0,4} |{1,15} | {2,19}", i, inputstring[i], score[i]);
+            }
+        }
 
+        public void RecognitionString(string inputstr)
+        {
+            isRecognition = false;
+            step = 0;
+            inputstring.Add(inputstr);
+            Recognition(score[0]);
+            if (isRecognition)
+            {
+                Console.WriteLine("\nСтрока успешно распознана:\n");
+                Print();
+            }
+            else Console.WriteLine("\nНевозможно распознать строку с помощью данной граматики\n");
+            inputstring.Clear();
+            score.RemoveRange(1, score.Count-1);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace MPautomat
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                string rule;
+                string recogstr;
+                Console.WriteLine("Введите начальный символ грамматики");
+                Automat automat = new Automat(Console.ReadLine());
+                Console.WriteLine("Введите правила грамматики. Каждое правило - на отдельной строке. Левая часть не должна повторяться\n" +
+                    "Пример строки: S->aB\n" +
+                    "Для символа пустой строки (эпсилон) зарезервирован символ 'e'.\n" +
+                    "Для завершения ввода введите пустую строку.");
+                for (; ; )
+                {
+                    rule = Console.ReadLine();
+                    if (rule == string.Empty) break;
+                    automat.AddRule(rule);
+                }
+                for (; ; )
+                {
+                    Console.WriteLine("Введите строку для распознования (оставьте строку пустой для выхода из программы)");
+                    recogstr = Console.ReadLine();
+                    if (recogstr == string.Empty) break;
+                    automat.RecognitionString(recogstr);
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
         }
     }
 }
